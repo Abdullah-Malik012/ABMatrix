f0b708a76a80bfd23bdb662ac9b725d3572378b3
Abdullah Malik <95423490+Abdullah-Malik012@users.noreply.github.com>
2024-08-10 02:26:44 +0500

    Update Header.jsx

--- src/components/Sections/Header.jsx
+++ src/components/Sections/Header.jsx
@@ -8,53 +8,51 @@ import bgvideo from "./bg.mov";
 export default function Header() {
   return (
     <Wrapper id="home">
-      <video autoPlay loop muted>
-        <source src={bgvideo} />
-      </video>
+      <VideoBackground autoPlay loop muted playsInline disablePictureInPicture>
+        <source src={bgvideo} type="video/mp4" />
+      </VideoBackground>
       <Content>
         <LeftSide>
           <AnimatedHeader>
-          <QuoteWrapper className="flexCenter darkBg radius8">
-            <QuotesWrapper>
-              <QuotesIcon />
-            </QuotesWrapper>
-            <QuoteText className="font15 whiteColor">
-              <em>
-                In the realm of code, dreams become functions, visions become
-                features, and aspirations transform into executable realities.
-              </em>
-            </QuoteText>
-          </QuoteWrapper>
+            <QuoteWrapper className="flexCenter darkBg radius8">
+              <QuotesWrapper>
+                <QuotesIcon />
+              </QuotesWrapper>
+              <QuoteText className="font15 whiteColor">
+                <em>
+                  In the realm of code, dreams become functions, visions become
+                  features, and aspirations transform into executable realities.
+                </em>
+              </QuoteText>
+            </QuoteWrapper>
           </AnimatedHeader>
-
         </LeftSide>
         <RightSide>
           <AnimatedHeader className="extraBold font60">
             Transforming Visions into Software Realities.
-          
-          <HeaderP className="font15 semiBold">
-            "Welcome to our world at AB-Matrix, where we specialize in
-            transforming your visions into tangible software realities. We
-            understand the power of ideas and the impact they can have on
-            businesses and lives. Through a fusion of creativity and
-            cutting-edge technology, we meticulously craft every line of code
-            to bring your concepts to life."
-          </HeaderP>
-          <BtnWrapper>
-            <Link
-              activeClass="active"
-              style={{ padding: "10px 15px" }}
-              to="services"
-              spy={true}
-              smooth={true}
-              offset={-80}
-            >
-              <FullButton
-                style={{ backgroundColor: "#08116A" }}
-                title="Let's Get Started"
-              />
-            </Link>
-          </BtnWrapper>
+            <HeaderP className="font15 semiBold">
+              "Welcome to our world at AB-Matrix, where we specialize in
+              transforming your visions into tangible software realities. We
+              understand the power of ideas and the impact they can have on
+              businesses and lives. Through a fusion of creativity and
+              cutting-edge technology, we meticulously craft every line of code
+              to bring your concepts to life."
+            </HeaderP>
+            <BtnWrapper>
+              <Link
+                activeClass="active"
+                style={{ padding: "10px 15px" }}
+                to="services"
+                spy={true}
+                smooth={true}
+                offset={-80}
+              >
+                <FullButton
+                  style={{ backgroundColor: "#08116A" }}
+                  title="Let's Get Started"
+                />
+              </Link>
+            </BtnWrapper>
           </AnimatedHeader>
         </RightSide>
       </Content>
@@ -83,22 +81,22 @@ const Wrapper = styled.section`
   background-position: center;
   text-align: center;
   position: relative;
-  
 
-  video {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    z-index: -1;
+ @media (max-width: 960px) {
+    margin-top:5%;
   }
 
-  @media (max-width: 960px) {
-  margin-top: 100px;
-    flex-direction: column;
-  }
+`;
+
+const VideoBackground = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+  pointer-events: none;  /* Prevent interaction with the video */
 `;
 
 const Content = styled.div`
@@ -120,7 +118,7 @@ const Content = styled.div`
 
 const RightSide = styled.div`
   width: 50%;
-    margin-top: 10%;
+  margin-top: 10%;
 
   @media (max-width: 960px) {
     width: 100%;
@@ -158,6 +156,8 @@ const BtnWrapper = styled.div`
   margin-top: 50px;
   @media (max-width: 960px) {
     width: 60%;
+      margin-top: -5%;
+
   }
   @media (max-width: 480px) {
     width: 80%;
