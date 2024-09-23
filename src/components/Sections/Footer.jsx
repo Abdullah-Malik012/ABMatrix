50252bcd9aec534d1e1ca4e18fcc3239fe496d13
Abdullah Malik <95423490+Abdullah-Malik012@users.noreply.github.com>
2024-09-23 22:02:19 +0500

    Update Footer.jsx

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 import { SiGmail, SiUpwork } from 'react-icons/si';
 import { FaLinkedin } from 'react-icons/fa';
 import { BsWhatsapp } from 'react-icons/bs';
-import { FaHome, FaEnvelope, FaPhone, FaPrint } from 'react-icons/fa';
 import FooterBanner from '../../components/Sections/footerbanner.jpg';  // Adjust the path as needed
 
-import { Link } from 'react-scroll'; // Import Link from react-scroll
+import { Link } from 'react-scroll';
 import mernlogo from '../../components/Sections/MERN-logo.png';
 import ailogo from '../../components/Sections/ailogo.png';
 import blockchainlogo from '../../components/Sections/blockchainlogo.png';
@@ -14,112 +13,132 @@ import dblogo from '../../components/Sections/dblogo.png';
 
 import CompanyLogo from '../../components/Sections/abmatrixnobg.png';  // Import your logo image
 
-export default function CustomFooter() {
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth < 768);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile;
+};
+
+const EmailIconLink = ({ isMobile }) => {
+  const emailHref = isMobile 
+    ? "mailto:abmatrix.co@gmail.com" 
+    : "https://mail.google.com/mail/?view=cm&fs=1&to=abmatrix.co@gmail.com";
+
   return (
-<>
-    <SocialSection>
+    <IconLink href={emailHref} target="_blank" rel="noopener noreferrer">
+      <SiGmail />
+    </IconLink>
+  );
+};
+
+export default function CustomFooter() {
+  const isMobile = useIsMobile();
+
+  return (
+    <>
+      <SocialSection>
         <div>
           <span>Get connected with us on social networks:</span>
         </div>
         <div>
-        <IconLink href="https://www.linkedin.com/company/abmatrixsolutions/" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin /></IconLink>
+          <IconLink href="https://www.linkedin.com/company/abmatrixsolutions/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </IconLink>
           <IconLink href="https://wa.me/923137753833" target="_blank" rel="noopener noreferrer">
-              <BsWhatsapp />
-            </IconLink>        
-            <IconLink
-        href="https://mail.google.com/mail/?view=cm&fs=1&to=abmatrix.co@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      ><SiGmail /></IconLink>
-           <IconLink href="https://www.upwork.com/freelancers/~0161c6d8726055c7c7" target="_blank" rel="noopener noreferrer"><SiUpwork /></IconLink>
+            <BsWhatsapp />
+          </IconLink>        
+          <EmailIconLink isMobile={isMobile} />
+          <IconLink href="https://www.upwork.com/freelancers/~0161c6d8726055c7c7" target="_blank" rel="noopener noreferrer">
+            <SiUpwork />
+          </IconLink>
         </div>
       </SocialSection>
-    <FooterContainer>
-      
 
-      <ContentSection>
-        <ContentRow>
-          <ContentCol>
-            <LogoWrapper>
-            <Link 
-        className="pointer flexNullCenter" 
-        to="home" // Use an ID that corresponds to the top of your page
-        smooth={true} 
-        duration={800} // Duration of the scroll animation
-    >
-        <img src={CompanyLogo} alt="home" />
-    </Link> 
-                    </LogoWrapper>
-          </ContentCol>
+      <FooterContainer>
+        <ContentSection>
+          <ContentRow>
+            <ContentCol>
+              <LogoWrapper>
+                <Link 
+                  className="pointer flexNullCenter" 
+                  to="home" 
+                  smooth={true} 
+                  duration={800}
+                >
+                  <img src={CompanyLogo} alt="home" />
+                </Link> 
+              </LogoWrapper>
+            </ContentCol>
 
-          <ContentCol>
-            <h6>Services</h6>
-            <ul>
-              <li><a href="/Services">Web Development</a></li>
-              <li><a href="/Services">Artificial Intelligence</a></li>
-              <li><a href="/Services">Databases</a></li>
+            <ContentCol>
+              <h6>Services</h6>
+              <ul>
+                <li><a href="/Services">Web Development</a></li>
+                <li><a href="/Services">Artificial Intelligence</a></li>
+                <li><a href="/Services">Databases</a></li>
+                <li><a href="/Services">Blockchain</a></li>
+              </ul>
+            </ContentCol>
 
-              <li><a href="/Services">Blockchain</a></li>
-            </ul>
-          </ContentCol>
+            <ContentCol>
+              <h6>Useful Links</h6>
+              <ul>
+                <li><a href="/Pricing">Pricing</a></li>
+                <li><a href="/Services">Services</a></li>
+                <li><a href="/Projects">Projects</a></li>
+                <li><a href="/Contact">Contact</a></li>
+              </ul>
+            </ContentCol>
 
-          <ContentCol>
-            <h6>Useful Links</h6>
-            <ul>
-              <li><a href="/Pricing">Pricing</a></li>
-              <li><a href="/Services">Services</a></li>
-              <li><a href="/Projects">Projects</a></li>
-              <li><a href="/Contact">Contact</a></li>
-            </ul>
-          </ContentCol>
+            <ContentCol>
+              <h6 style={{ marginBottom: '35px'}}>Technology</h6>
+              <ul style={{ display: 'flex', listStyle: 'none', padding: 0, justifyContent: 'center' }}>
+                <li style={{ margin: '0 15px', textAlign: 'center' }}>
+                  <img src={mernlogo} alt="MERN" style={{ width: '80px', height: 'auto' }} />
+                  <p style={{ marginTop: '5px', fontSize: '14px' }}>MERN</p>
+                </li>
+                <li style={{ margin: '0 15px', textAlign: 'center' }}>
+                  <img src={dblogo} alt="MongoDB" style={{ width: '35px', height: 'auto' }} />
+                  <p style={{ marginTop: '5px', fontSize: '14px' }}>SQL & NoSQL</p>
+                </li>
+                <li style={{ margin: '0 15px', textAlign: 'center' }}>
+                  <img src={ailogo} alt="AI" style={{ width: '35px', height: 'auto' }} />
+                  <p style={{ marginTop: '5px', fontSize: '14px' }}>AI</p>
+                </li>
+                <li style={{ margin: '0 15px', textAlign: 'center' }}>
+                  <img src={blockchainlogo} alt="Blockchain" style={{ width: '50px', height: 'auto' }} />
+                  <p style={{ marginTop: '5px', fontSize: '14px' }}>Blockchain</p>
+                </li>
+              </ul>
+            </ContentCol>
+          </ContentRow>
+        </ContentSection>
 
-          <ContentCol>
-  <h6 style={{ marginBottom: '35px'}}>Technology</h6>
-  <ul style={{ display: 'flex', listStyle: 'none', padding: 0, justifyContent: 'center' }}>
-    <li style={{ margin: '0 15px', textAlign: 'center' }}>
-      <img src={mernlogo} alt="MERN" style={{ width: '80px', height: 'auto' }} />
-      <p style={{ marginTop: '5px', fontSize: '14px' }}>MERN</p>
-    </li>
-
-    <li style={{ margin: '0 15px', textAlign: 'center' }}>
-      <img src={dblogo} alt="MongoDB" style={{ width: '35px', height: 'auto' }} />
-      <p style={{ marginTop: '5px', fontSize: '14px' }}>SQL & NoSQL</p>
-    </li>
-
-    <li style={{ margin: '0 15px', textAlign: 'center' }}>
-      <img src={ailogo} alt="AI" style={{ width: '35px', height: 'auto' }} />
-      <p style={{ marginTop: '5px', fontSize: '14px' }}>AI</p>
-    </li>
-
-    <li style={{ margin: '0 15px', textAlign: 'center' }}>
-      <img src={blockchainlogo} alt="Blockchain" style={{ width: '50px', height: 'auto' }} />
-      <p style={{ marginTop: '5px', fontSize: '14px' }}>Blockchain</p>
-    </li>
-  </ul>
-</ContentCol>
-
-
-        </ContentRow>
-      </ContentSection>
-
-      <FooterBottom>
-        <p>© 2024 Copyright: <a>AB Matrix</a></p>
-      </FooterBottom>
-    </FooterContainer>
-
+        <FooterBottom>
+          <p>© 2024 Copyright: <a>AB Matrix</a></p>
+        </FooterBottom>
+      </FooterContainer>
     </>
   );
 }
 
 const FooterContainer = styled.footer`
-  position: relative; /* Required for the overlay */
+  position: relative;
   background-image: url(${FooterBanner});
-  background-size: cover; /* Ensures the image covers the entire footer */
-  background-position: center; /* Centers the image */
-  color: #ffffff;  /* White text */
+  background-size: cover;
+  background-position: center;
+  color: #ffffff;
   padding-top: 20px;
-  padding-bottom: 40px;  /* Increased bottom padding to ensure nothing is hidden */
+  padding-bottom: 40px;
   font-size: 14px;
   text-align: center;
 
@@ -130,20 +149,20 @@ const FooterContainer = styled.footer`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0, 0.8); /* Light black overlay */
-    z-index: 1; /* Ensure the overlay is above the background but below the content */
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 1;
   }
 
   > * {
-    position: relative; /* Bring child elements above the overlay */
+    position: relative;
     z-index: 2;
   }
 `;
 
 const SocialSection = styled.section`
   padding: 20px;
-background: radial-gradient(circle, #010121, #000000);
-color: white;
+  background: radial-gradient(circle, #010121, #000000);
+  color: white;
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   display: flex;
   justify-content: space-between;
@@ -240,9 +259,9 @@ const ContentCol = styled.div`
 `;
 
 const LogoWrapper = styled.div`
-  display: flex;               /* Use flexbox */
-  justify-content: center;     /* Center horizontally */
-  align-items: center;         /* Center vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   
   img {
     max-width: 150px;
@@ -256,48 +275,17 @@ const LogoWrapper = styled.div`
   }
 `;
 
-
 const FooterBottom = styled.div`
   background-color: rgba(255, 255, 255, 0.1);
   padding: 15px 0;
-  font-size: 12px;
-  color: #ffffff;
   text-align: center;
 
   p {
     margin: 0;
-    padding: 0 15px;
-
-    a {
-      color: inherit;
-      font-weight: bold;
-      text-decoration: none;
-
-      &:hover {
-        color: #ffffff;
-      }
-    }
   }
 
-  @media (max-width: 768px) {
-        margin-bottom: 20%;
-  
-  padding: 10px 0;
-    font-size: 10px;
-
-    p {
-      padding: 0 10px;
-    }
-  }
-
-  @media (max-width: 480px) {
-    padding: 18px 0;
-    font-size: 9px;
-      margin-bottom: 30%;
-
-
-    p {
-      padding: 0 8px;
-    }
+  a {
+    color: #ffffff;
+    text-decoration: none;
   }
 `;
